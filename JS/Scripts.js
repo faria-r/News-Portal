3ddafb3939db43c06a7f06a3d276fc3cd819f821
faria-r <fariacse11@gmail.com>
2022-09-03 14:21:44 +0600

    blog section added

--- JS/Scripts.js
+++ JS/Scripts.js
@@ -10,14 +10,56 @@ const loadCategories = ()=>{
     const categoryContainer = document.getElementById('Category-container');
     categories.forEach(category =>{
     const categoryDiv = document.createElement('div');
+    categoryDiv.classList.add('px-4')
     categoryDiv.innerHTML= 
     `
-    <h2>${category.category_name}</h2>
-    `;
+    <button onclick="loadNews('')">${category.category_name}</button>
+    `; //${.category_id}
     categoryContainer.appendChild(categoryDiv);
     })
     }
+//load News accroding to category
+const loadNews = () => {
+    const url = `https://openapi.programming-hero.com/api/news/category/01`
+    fetch(url)
+    .then(res => res.json())
+    // .then(data => showNews(data.data.news_category))
+    .then(data => showNews(data.data))
+}
 
+//Show news in UI
+const showNews = news =>{
+    const newsContainer = document.getElementById('news-container');
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('card');
+        newsDiv.innerHTML= 
+        `
+        <div class="row g-0">
+        <div class="col-md-4">
+          <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">${news.title}</h5>
+            <p class="card-text">${news.details}</p>
+            <p class="card-text">${news.details}</p>
+          </div>
+          <div class="d-flex">
+                <div class="d-flex'>
+                <img src="" class="img-fluid rounded-start" alt="...">
+               
+                </div>
+          </div>
+        </div>
+      </div>   
+        `;
+        newsContainer.appendChild(newsDiv);
+
+}
+// loadNews();
+
+//calling categories Loader Function
 loadCategories();
 
 
+//${news.author.img}  <p>${news.author.name}</p>
