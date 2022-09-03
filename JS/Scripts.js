d481282fc415e11f5e14247bf0e293f21e2ac386
faria-r <fariacse11@gmail.com>
2022-09-04 00:29:13 +0600

    modal data loaded dynamically

--- JS/Scripts.js
+++ JS/Scripts.js
@@ -12,18 +12,20 @@ const displayCategories = categories => {
         const categoryDiv = document.createElement('div');
         categoryDiv.classList.add('px-4')
         categoryDiv.innerHTML =
-            `
-    <button onclick="loadNews('${category.category_id}')">${category.category_name}</button>
-    `;
+            `<button onclick="loadNews('${category.category_id}')">${category.category_name}
+        </button>`;
         categoryContainer.appendChild(categoryDiv);
     })
 }
+//calling categories Loader Function
+loadCategories();
+
 //load News accroding to category
 const loadNews = (id) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
     fetch(url)
         .then(res => res.json())
-        .then( data => showNews(data.data ? data.data : 'News Not Found'));
+        .then(data => showNews(data.data));
 }
 //Show news in UI
 const showNews = allnews => {
@@ -58,7 +60,7 @@ const showNews = allnews => {
                     </div>
                     </div>
                     <div class="text-center mt-3">
-                    <button onclick="loadNewsDetaile('')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Show Detailes</button>
+                    <button onclick="loadNewsDetailes('${news._id}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Show Detailes</button>
                     </div>
         `;
         newsContainer.appendChild(newsDiv);
@@ -68,8 +70,7 @@ const showNews = allnews => {
 
 loadNews();
 
-//calling categories Loader Function
-loadCategories();
+
 //spinnner function 
 const toggleSpinner = isLoading => {
     const loaderSection = document.getElementById('loader');
@@ -81,26 +82,27 @@ const toggleSpinner = isLoading => {
     }
 }
 
-
-//modal
-const loadNewsDetailes = async id =>{
-    const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+//modal for details news
+const loadNewsDetailes = async id => {
+    const url = `https://openapi.programming-hero.com/api/news/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    displaynewsDetailes(data.data);
-;}
-
+    displaynewsDetailes (data.data);
+    ;
+}
+//function to display Modal
 const displaynewsDetailes = news => {
-    console.log(news);
     const ModalTitle = document.getElementById('exampleModalLabel');
-    ModalTitle.innerText = news.title;
-    const phoneDetailes = document.getElementById('newsdetailes');
-    phoneDetailes.innerHTML= `
-    <p>Release Date: ${news.title ?news.title: 'no title Found'}</p>
-    <p>Bluetooth:${phone.others?phone.others.Bluetooth : 'No Detail Founds'}</p>
-    <p>Storage:${phone.mainFeatures?phone.mainFeatures.storage : 'No Storage Detail Founds'}</p>
-    <p>Sensors:${phone.mainFeatures?phone.mainFeatures.sensors[0] : 'No Sensor Detail Founds'}</p>
-    `;
-    ModalTitle.appendChild(phoneDetailes);
-    
+    ModalTitle.innerHTML='';
+    news.forEach(allnews =>{
+        const modalDiv = document.createElement('div');
+        modalDiv.classList.add('modal-body');
+        modalDiv.innerHTML= `
+        <img src="${allnews.author.img ? allnews.author.img : 'image Not Available'}" class="img-fluid rounded" style="height:50px;width:50px;">
+        <p>Author Name:${allnews.author.name ? allnews.author.name: 'Name Not Available'} </p>
+        <p>Total Veiw: ${allnews.total_view ? allnews.total_view : 'No Veiw'}</p>
+        <p>Published Date: ${allnews.author.published_date ? allnews.author.published_date : 'Date Not Available'}</p>
+        `;
+        ModalTitle.appendChild(modalDiv);
+    })
 }
