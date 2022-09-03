48d71c5c7789eb830736508a6abf69d881c2f49c
faria-r <fariacse11@gmail.com>
2022-09-03 22:26:13 +0600

    modal added

--- JS/Scripts.js
+++ JS/Scripts.js
@@ -4,7 +4,6 @@ const loadCategories = async () => {
     const res = await fetch(url);
     const data = await res.json();
     displayCategories(data.data.news_category);
-
 }
 //Display categories in UI
 const displayCategories = categories => {
@@ -19,52 +18,57 @@ const displayCategories = categories => {
         categoryContainer.appendChild(categoryDiv);
     })
 }
-
-
 //load News accroding to category
 const loadNews = (id) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => showNews(data.data))
+        .then( data => showNews(data.data ? data.data : 'News Not Found'));
 }
-
 //Show news in UI
 const showNews = allnews => {
     const newsContainer = document.getElementById('news-container');
     newsContainer.innerHTML = '';
     allnews.forEach(news => {
         const newsDiv = document.createElement('div');
-        newsDiv.classList.add('card');
-
+        newsDiv.classList.add('card', 'mb-5', 'p-4', 'bg-secondary');
         newsDiv.innerHTML =
-            `
-        <div class="row g-1">
+            `<div class="row g-1">
         <div class="col-md-4">
-          <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+        <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
         </div>
         <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-          </div>
+        <div class="card-body">
+        <h5 class="card-title">${news.title}</h5>
+        <p style="width:100%;
+        height: 70px;text-overflow: ellipsis; display: -webkit-box;
+        -webkit-line-clamp: 3;
+        -webkit-box-orient: vertical;
+        overflow: hidden;" class="card-text d-block">${news.details}</p>
         </div>
-      </div>
+          <div class = "d-flex justify-content-evenly mt-5">
+                    <div class="d-flex">
+                    <img src="${news.author.img}" class="img-fluid rounded" style="height:50px;width:50px;">
+                    <p class="ps-4">${news.author.name ? news.author.name : 'Name Not Found'}</p>
+                    </div>
+                    <div class="d-flex">
+                    <i class="fa-thin fa-eye"></i>
+                    <p class="ps-4">${news.total_view}</p>
+                    </div>
+                    </div>
+                    <div class="text-center mt-3">
+                    <button onclick="loadNewsDetaile('')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Show Detailes</button>
+                    </div>
         `;
         newsContainer.appendChild(newsDiv);
     })
 
 }
+
 loadNews();
 
 //calling categories Loader Function
 loadCategories();
-
-
-
-
-
 //spinnner function 
 const toggleSpinner = isLoading => {
     const loaderSection = document.getElementById('loader');
@@ -76,5 +80,26 @@ const toggleSpinner = isLoading => {
     }
 }
 
-// <img src="${news.author.img}" class="img-fluid rounded-start" alt="...">
-//<p>${news.author.name}</p>
+
+//modal
+const loadNewsDetailes = async id =>{
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displaynewsDetailes(data.data);
+;}
+
+const displaynewsDetailes = news => {
+    console.log(news);
+    const ModalTitle = document.getElementById('exampleModalLabel');
+    ModalTitle.innerText = news.title;
+    const phoneDetailes = document.getElementById('newsdetailes');
+    phoneDetailes.innerHTML= `
+    <p>Release Date: ${news.title ?news.title: 'no title Found'}</p>
+    <p>Bluetooth:${phone.others?phone.others.Bluetooth : 'No Detail Founds'}</p>
+    <p>Storage:${phone.mainFeatures?phone.mainFeatures.storage : 'No Storage Detail Founds'}</p>
+    <p>Sensors:${phone.mainFeatures?phone.mainFeatures.sensors[0] : 'No Sensor Detail Founds'}</p>
+    `;
+    ModalTitle.appendChild(phoneDetailes);
+    
+}
