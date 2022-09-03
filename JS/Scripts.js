8ad583313a1817c511b42bc7f6d5c969db672f3f
faria-r <fariacse11@gmail.com>
2022-09-03 17:26:16 +0600

    displayed all news section

--- JS/Scripts.js
+++ JS/Scripts.js
@@ -1,66 +1,59 @@
 //load categories Api
-const loadCategories = ()=>{
+const loadCategories = async () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`
-    fetch(url)
-    .then(res =>res.json())
-    .then(data => displayCategories(data.data.news_category))
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCategories(data.data.news_category);
+
 }
 //Display categories in UI
-    const displayCategories = categories => {
+const displayCategories = categories => {
     const categoryContainer = document.getElementById('Category-container');
-    categories.forEach(category =>{
-    const categoryDiv = document.createElement('div');
-    categoryDiv.classList.add('px-4')
-    categoryDiv.innerHTML= 
-    `
+    categories.forEach(category => {
+        const categoryDiv = document.createElement('div');
+        categoryDiv.classList.add('px-4')
+        categoryDiv.innerHTML =
+            `
     <button onclick="loadNews('${category.category_id}')">${category.category_name}</button>
-    `; 
-    categoryContainer.appendChild(categoryDiv);
+    `;
+        categoryContainer.appendChild(categoryDiv);
     })
-    }
-
-
-
-
-
+}
 
 
 //load News accroding to category
 const loadNews = (id) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
     fetch(url)
-    .then(res => res.json())
-    .then(data => showNews(data.data[0]))
+        .then(res => res.json())
+        .then(data => showNews(data.data))
 }
 
 //Show news in UI
-const showNews = news => {
-    console.log(news);
+const showNews = allnews => {
     const newsContainer = document.getElementById('news-container');
+    newsContainer.innerHTML = '';
+    allnews.forEach(news => {
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('card');
-        newsDiv.innerHTML= 
-        `
-        <div class="row g-0">
+
+        newsDiv.innerHTML =
+            `
+        <div class="row g-1">
         <div class="col-md-4">
           <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
         </div>
         <div class="col-md-8">
           <div class="card-body">
-            <h5 class="card-title">${news.title}</h5>
-            <p class="card-text">${news.details}</p>
-            <p class="card-text">${news.details}</p>
-          </div>
-          <div class="d-flex">
-                <div class="d-flex'>
-                <img src="" class="img-fluid rounded-start" alt="...">
-               
-                </div>
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
           </div>
         </div>
-      </div>   
+      </div>
         `;
         newsContainer.appendChild(newsDiv);
+    })
 
 }
 loadNews();
@@ -69,4 +62,19 @@ loadNews();
 loadCategories();
 
 
-//${news.author.img}  <p>${news.author.name}</p>
+
+
+
+//spinnner function 
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if (isLoading === true) {
+        loaderSection.classList.remove('d-none')
+    }
+    else {
+        loaderSection.classList.add('d-none')
+    }
+}
+
+// <img src="${news.author.img}" class="img-fluid rounded-start" alt="...">
+//<p>${news.author.name}</p>
